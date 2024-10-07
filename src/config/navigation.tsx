2965951b9fdb3c19e7eda51db68635dcd9c5dc63
Slava Pogosov <pogoslav1@gmail.com>
2024-10-07 17:19:52 +0300

    ADDED: prettier config; UPDATED: formatted code

--- src/config/navigation.tsx
+++ src/config/navigation.tsx
@@ -1,7 +1,7 @@
 // src/config/navigation.ts
 export interface NavigationItem {
-    name: string;
-    path: string;
+    name: string
+    path: string
 }
 
 // Определяем все страницы, которые будут отображаться в навигации
@@ -9,4 +9,4 @@ export const navigationItems: NavigationItem[] = [
     { name: 'НОВОСТИ', path: '/' },
     { name: 'МАТЧИ', path: '/about' },
     { name: 'КОМАНДА', path: '/contact' },
-];
+]
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,31 +1,36 @@
 // src/routes.tsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainPage from './pages/Main/MainPage';
-import PageWithNavBar from './layouts/PageWithNavBar';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import MainPage from './pages/Main/MainPage'
+import PageWithNavBar from './layouts/PageWithNavBar'
 
 const AppRoutes: React.FC = () => {
     return (
         <Router>
             <Routes>
                 <Route
-                    path="/"
+                    path='/'
                     element={
                         <PageWithNavBar>
                             <MainPage />
                         </PageWithNavBar>
                     }
                 />
-                <Route path="/about" element={<PageWithNavBar>
-                    <div>About Page</div>
-                </PageWithNavBar>}/>
-                <Route path="/contact" element={<div>Contact Page</div>} />
+                <Route
+                    path='/about'
+                    element={
+                        <PageWithNavBar>
+                            <div>About Page</div>
+                        </PageWithNavBar>
+                    }
+                />
+                <Route path='/contact' element={<div>Contact Page</div>} />
                 {/* Добавь другие маршруты здесь */}
                 {/*<Route path="/about" element={<div>About Page</div>} />*/}
                 {/*<Route path="/contact" element={<div>Contact Page</div>} />*/}
             </Routes>
         </Router>
-    );
-};
+    )
+}
 
-export default AppRoutes;
+export default AppRoutes
